80af91f1df68d55c876146a27350c5ea95128e41
Evil-Coder-11212 <99388161+Evil-Coder-11212@users.noreply.github.com>
2022-03-25 14:37:36 +0530

    Add files via upload

--- To Do List/js/main.js
+++ To Do List/js/main.js
@@ -1,1 +1,32 @@
-// To Do Tomorrow
+// Islzing Variables
+const taskInput = document.getElementById("add-task-input");
+const addBtn = document.getElementById("add-btn");
+
+const addingElements = (value) => {
+  let error = true;
+  let errorMessage = "Please put a value grater 0";
+  const ul = document.getElementById("append-lists");
+  const listText = document.getElementsByClassName("lists-task-text");
+  if (value.length >= 1) {
+    const li = `
+                    <li>
+                        <input type="checkbox" name="list" class="lists" />
+                        <p class="lists-task-text">${value}</p>
+                    </li>
+                `;
+    ul.innerHTML += li;
+  } else {
+    alert("Please put a value grater 0");
+  }
+};
+
+// Calling Function
+addBtn.addEventListener("click", () => {
+  addingElements(taskInput.value);
+});
+
+// Problem
+// Delete Button
+// Delete All Button
+// Add the task in local storage
+// When the check box is clicked it should turn the list to -----
