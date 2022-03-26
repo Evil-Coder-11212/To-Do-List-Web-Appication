3b0f9376493ee4649e0c3cb1b252f720fc266625
Evil-Coder-11212 <99388161+Evil-Coder-11212@users.noreply.github.com>
2022-03-26 10:25:23 +0530

    Added delete all button
    Left:
    Delete button
    local storage saving

--- To Do List/js/main.js
+++ To Do List/js/main.js
@@ -1,20 +1,28 @@
 // Islzing Variables
 const taskInput = document.getElementById("add-task-input");
 const addBtn = document.getElementById("add-btn");
+const deleteAllBtn = document.getElementById("delete-all-btn");
+const ul = document.getElementById("append-lists");
+let task = [];
+
+const deleteAll = () => {
+  task = [];
+  ul.innerHTML = "";
+};
 
 const addingElements = (value) => {
   let error = true;
   let errorMessage = "Please put a value grater 0";
-  const ul = document.getElementById("append-lists");
-  const listText = document.getElementsByClassName("lists-task-text");
   if (value.length >= 1) {
     const li = `
-                    <li>
-                        <input type="checkbox" name="list" class="lists" />
-                        <p class="lists-task-text">${value}</p>
-                    </li>
-                `;
+                        <li>
+                            <input type="checkbox" name="list" class="lists" />
+                            <p class="lists-task-text">${value}</p>
+                        </li>
+                    `;
+    task.push(li);
     ul.innerHTML += li;
+    console.log(task);
   } else {
     alert("Please put a value grater 0");
   }
@@ -25,8 +33,11 @@ addBtn.addEventListener("click", () => {
   addingElements(taskInput.value);
 });
 
+deleteAllBtn.addEventListener("click", () => {
+  deleteAll();
+});
+
 // Problem
 // Delete Button
-// Delete All Button
 // Add the task in local storage
 // When the check box is clicked it should turn the list to -----
